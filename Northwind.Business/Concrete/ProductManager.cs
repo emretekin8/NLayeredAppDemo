321043f962ac41dc5e38ddea56aff110558b7f04
emretekin8 <emretekin601@gmail.com>
2022-07-28 10:17:21 +0300

    Add project files.

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,76 @@
+﻿using Northwind.Business.Abstract;
+using FluentValidation;
+using Northwind.Business.ValidationRules.FluentValidation;
+using Northwind.DataAccess.Abstract;
+using Northwind.DataAccess.Concrete;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationException = FluentValidation.ValidationException;
+using Northwind.Business.Utilities;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            //ValidationTool.Validate(new ProductValidator(), product);
+            ProductValidator productValidator = new ProductValidator();
+            productValidator.Validate(product);
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            try
+            {
+                _productDal.Delete(product);
+            }
+            catch
+            {
+                throw new Exception("Delete Failed!");
+            }
+
+        }
+
+        public List<Product> GetAll()
+        {
+            //business code
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetProductsByCategory(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public List<Product> GetProductsByPorductName(string productName)
+        {
+            return _productDal.GetAll(p => p.ProductName.ToLower().Contains(productName.ToLower()));
+
+        }
+
+        public void Update(Product product)
+        {
+            //ValidationTool.Validate(new ProductValidator(), product);
+            ProductValidator productValidator = new ProductValidator();
+            productValidator.Validate(product);
+            _productDal.Update(product);
+        }
+    }
+}
---
+++ Northwind.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.ValidationRules.FluentValidation
+{
+    internal class ProductValidator : AbstractValidator<Product>
+    {
+        //fluent validation 
+        public ProductValidator()
+        {
+
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Product Name can't be empty");
+            RuleFor(p => p.CategoryId).NotEmpty();
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.QuantityPerUnit).NotEmpty();
+            RuleFor(p => p.UnitsInStock).NotEmpty();
+
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitsInStock).GreaterThanOrEqualTo((short)0);
+            RuleFor(p => p.UnitPrice).GreaterThan(10).When(p => p.CategoryId == 2);
+
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Product name must starts with 'A'"); //create your own validation rule with a method
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
